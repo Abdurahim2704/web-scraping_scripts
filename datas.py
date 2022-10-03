446c33d9a40dd8bc2018653ed2e0c21106e2eb45
Abdurahim Nasirdinov <96531979+Abdurahim2704@users.noreply.github.com>
2022-10-03 14:24:50 +0500

    Delete datas.py

--- datas.py
+++
@@ -1,8 +0,0 @@
-headers = {
-            # 'Accept-Encoding': 'gzip, deflate, sdch',
-            'Accept-Language': 'en-US,en;q=0.8',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Referer': 'http://www.wikipedia.org/',
-            'Connection': 'keep-alive',
-        }
